8417106866f09edd63beb7c9d6e4f5bec438c9fa
Helloworld <1326521+LYZhelloworld@users.noreply.github.com>
2021-02-01 12:26:51 +0800

    Add drag/drop style

--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,31 @@ import { generatePDFName, getImages } from './fileutils/fileutils'
 import { createPDF } from './generator/generator'
 
 function generatePDF(directory: string) {
-	getImages(directory).then(images => {
-		if (images.length === 0) return
-		let pdf = generatePDFName(directory)
-		createPDF(pdf, images, () => {
-			//console.log(filename)
-			// TODO: progress
-		}).then(() => {
-			// TODO: finished
-		})
-	})
+    getImages(directory).then(images => {
+        if (images.length === 0) return
+        let pdf = generatePDFName(directory)
+        createPDF(pdf, images, () => {
+            //console.log(filename)
+            // TODO: progress
+        }).then(() => {
+            // TODO: finished
+        })
+    })
+}
+
+function toogleDragOverlayer(show: boolean) {
+    let overlayer = document.getElementById("drag-overlayer")
+    if (show) {
+        overlayer!.style.display = ""
+    } else {
+        overlayer!.style.display = "none"
+    }
 }
 
 document.addEventListener('drop', (event) => {
     event.preventDefault()
     event.stopPropagation()
+    toogleDragOverlayer(false)
     let files = event.dataTransfer?.files
     if (!files) return
     for (let i = 0; i < files.length; i++) {
@@ -29,4 +39,14 @@ document.addEventListener('drop', (event) => {
 document.addEventListener('dragover', (event) => {
     event.preventDefault()
     event.stopPropagation()
+})
+
+document.addEventListener('dragenter', (event) => {
+    if (event.target === document.getElementById('drag-area'))
+        toogleDragOverlayer(true)
+})
+
+document.addEventListener('dragleave', (event) => {
+    if (event.target === document.getElementById('drag-area'))
+        toogleDragOverlayer(false)
 })
